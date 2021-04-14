9f6f6f381e97e3d4bde7be522171a4553b7dbfc1
douzi <249078602@qq.com>
2021-04-14 17:00:14 +0800

    Create index.vue

---
+++ src/views/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        index
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OriginIndex',
+    components: { ComponentName },
+    directives: { DirectiveName },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
