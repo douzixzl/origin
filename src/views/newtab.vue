4da8c3c9772641e95c15c409d5caf93d23634abd
douzi <249078602@qq.com>
2021-04-14 16:05:32 +0800

    dz
    新增文件

---
+++ src/views/newtab.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OriginNewtab',
+
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
