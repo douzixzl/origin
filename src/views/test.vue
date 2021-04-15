1791d8d02e7b048c4bde6d22579416045ea4671f
douzi <249078602@qq.com>
2021-04-15 08:47:24 +0800

    dz
    合并分支

---
+++ src/views/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        哈哈
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OriginTest',
+    components: { ComponentName },
+    directives: { DirectiveName },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
